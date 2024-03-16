a508d49c891864015c9d0ddd79fc5e134e09ee3f
Jayshree <jayshreebshirsat16@gmail.com>
2024-03-16 16:53:25 +0530

    commit

--- app/src/main/java/com/example/mvvmpractice/livedataexample/LiveDataMainActivity.kt
+++ app/src/main/java/com/example/mvvmpractice/livedataexample/LiveDataMainActivity.kt
@@ -5,28 +5,36 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.mvvmpractice.R
+import com.example.mvvmpractice.databinding.ActivityLiveDataMainBinding
+import com.example.mvvmpractice.databinding.ActivityMainDatabindingBinding
 
 class LiveDataMainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityLiveDataMainBinding
     lateinit var mainViewModel: MainViewModel
-    private val factsTextview : TextView
-        get() = findViewById(R.id.factsTextView)
-
-    private val btnUpdate: Button
-        get() = findViewById(R.id.btnUpdate)
+//    private val factsTextview : TextView
+//        get() = findViewById(R.id.factsTextView)
+//
+//    private val btnUpdate: Button
+//        get() = findViewById(R.id.btnUpdate)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_live_data_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_live_data_main)
+//        setContentView(R.layout.activity_live_data_main)
         mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
         mainViewModel.factLiveData.observe(this, Observer {
-            factsTextview.text = it
+            binding.factsTextView.text = it
             Log.d("TAG", "onCreate: "+it)
         })
+        binding.mainViewModel = mainViewModel
 
-        btnUpdate.setOnClickListener {
-            mainViewModel.updateLiveData()
-        }
+
+//        btnUpdate.setOnClickListener {
+//            mainViewModel.updateLiveData()
+//        }
     }
 }
